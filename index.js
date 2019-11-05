668aea5c319bf94b254278e92620399a1e538450
Robert Parsons <rmpcoding@gmail.com>
2019-11-04 17:48:34 -0800

    Added two AJAX calls. Need to add third AJAX call regarding five-day forecast

---
+++ index.js
@@ -0,0 +1,64 @@
+console.log("hello world");
+$(document).ready(function() {
+
+var baseUrl = "http://api.openweathermap.org/data/2.5/"
+var state = "texas" //change with user input.
+var city = "austin" //change with user input.
+var apiKey = "a059151d000029215400bdaa7965fbc2";
+var temp; 
+var humidity;
+var windSpeed;
+var uvIndex;
+
+var lat = 34.06; //change with user input
+var long = -118.44; //change with user input
+var uvUrl = baseUrl + "uvi?" + "lat=" + lat + "&lon=" + long + "&units=imperial&appid=" +  apiKey;
+var queryUrl = baseUrl + "weather?" + "q=" + city + "," + state + "&units=imperial&appid=" + apiKey;
+
+var fiveDayForecast; // temperature && humidity only
+
+
+
+
+// ajax call for current city weather, excluding uv.
+$.ajax({
+  url: queryUrl,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+  console.log(response.main.humidity); //humidity
+  console.log(response.main.temp); //temp
+  console.log(response.wind.speed);
+  console.log(response.coord);
+  console.log(response.coord.lat);
+  console.log(response.coord.lon);
+  return response;
+});
+
+// ajax call for current uv value. Needs lat/long coordinates (probably from current city weather) in order to work.
+$.ajax({
+  url: uvUrl,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+  console.log(response.value); //value of the u.v.
+});
+
+
+// ajax call for five-day weather forecast.
+// $.ajax({
+//   url: "",
+//   method: "GET"
+// })
+
+
+
+
+// temperature
+// humidity
+// wind speed
+// uv index
+// 5-day forecast
+
+
+}); // end of document.ready function. do not delete.
