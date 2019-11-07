b1025e9de669fa20008bf16762150cfab2043dfd
Robert Parsons <rmpcoding@gmail.com>
2019-11-06 19:43:50 -0800

    Added current temperature card, as well as search button functionality

--- index.js
+++ index.js
@@ -1,136 +1,146 @@
 $(document).ready(function() {
+  // create function on submit even to store text into variable and into local storage.
 
-// create function on submit even to store text into variable and into local storage. 
-
-  var searchButton = document.body.querySelector(".search-input")
+  var searchButton = document.body.querySelector(".search-input");
   console.log(searchButton);
-  
+
   searchButton.addEventListener("keydown", function(enter) {
     if (enter.keyCode === 13) {
-      console.log(searchButton.value);
+      searchButton = searchButton.value;
+    } else {
+      return;
     }
-  });
 
-  console.log(searchButton);
+    console.log(searchButton);
 
-  var baseUrl =
-    "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/";
-  var state = "california"; //change with user input.
-  var city = "pasadena"; //change with user input.
-  var apiKey = "a059151d000029215400bdaa7965fbc2";
-  console.log(city);
+    var baseUrl =
+      "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/";
+    var city = searchButton; //change with user input.
+    var apiKey = "a059151d000029215400bdaa7965fbc2";
+    console.log(city);
 
-  var lat = 34.06; //change with user input
-  var long = -118.44; //change with user input
+    var lat = 34.06; //change with user input
+    var long = -118.44; //change with user input
 
-  var queryUrl =
-    baseUrl +
-    "weather?" +
-    "q=" +
-    city +
-    "," +
-    state +
-    "&units=imperial&appid=" +
-    apiKey;
-  var uvUrl =
-    baseUrl +
-    "uvi?" +
-    "lat=" +
-    lat +
-    "&lon=" +
-    long +
-    "&units=imperial&appid=" +
-    apiKey;
-  var forecastUrl =
-    baseUrl +
-    "forecast?" +
-    "q=" +
-    city +
-    "," +
-    state +
-    "&units=imperial&appid=" +
-    apiKey;
+    // list of icons. More at end of page. Need to work on this as noted at bottom of page.
+    var sunIcon = `<i class="fas fa-sun"></i>`;
+    var cloudIcon = `<i class="fas fa-cloud"></i>`;
+
+    var queryUrl =
+      baseUrl + "weather?" + "q=" + city + "&units=imperial&appid=" + apiKey;
+    var uvUrl =
+      baseUrl +
+      "uvi?" +
+      "lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&units=imperial&appid=" +
+      apiKey;
+    var forecastUrl =
+      baseUrl + "forecast?" + "q=" + city + "&units=imperial&appid=" + apiKey;
 
     console.log(queryUrl);
-  // ajax call function for multiple URLs
+    // ajax call function for multiple URLs
 
-  function ajaxCall(weatherUrl) {
-    $.ajax({
-      url: weatherUrl,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      generator(response);
-    });
-  }
-
-  ajaxCall(queryUrl);
-  ajaxCall(forecastUrl);
-  ajaxCall(uvUrl);
-
-  function generator(data) {
-    if (data.coord) {
-      var lat = data.coord.lat;
-      var long = data.coord.lon;
-      console.log(lat);
-      console.log(long);
+    function ajaxCall(weatherUrl) {
+      $.ajax({
+        url: weatherUrl,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        generator(response);
+      });
     }
 
-    if (data.main) {
-      var temp = data.main.temp;
-      var humidity = data.main.humidity;
-      var wind = data.wind.speed;
+    ajaxCall(queryUrl);
+    ajaxCall(forecastUrl);
+    ajaxCall(uvUrl);
 
-      console.log(temp);
-      console.log(humidity);
-      console.log(wind);
-    }
+    function generator(data) {
+      if (data.coord) {
+        var lat = data.coord.lat;
+        var long = data.coord.lon;
+        console.log(lat);
+        console.log(long);
+      }
 
-    if (data.list) {
-      console.log(data.list);
-      // var temp = data.main.temp;
-      // var humidity = data.main.humidity;
-      for (var i = 4; i < data.list.length; i += 8) {
-        console.log(data.list[i]);
-        var temp = data.list[i].main.temp;
-        var date = data.list[i].dt_txt;
-        date = moment(date).format('dddd');
-        var humidity = data.list[i].main.humidity;
-        var body = $("body");
-        var cardRow = $(".five-day-row");
-        var icon = `<i class="fas fa-temperature-high"></i>`
-        var card = $("<div>").html(
-          `<div class="card flex-row" style="width: 10rem;">
-        <div class="card-body card-style">
-        ${icon}
-        <h5 class="card-title">${date}</h5>
-        <p class="card-text card-style">
+      if (data.main) {
+        console.log(data.main);
+        var temp = data.main.temp;
+        var humidity = data.main.humidity;
+        var wind = data.wind.speed;
+        date = moment(date).format("dddd");
+
+        var currentTempRow = $(".current-temp-row");
+
+        var card = $("<div class='card col-sm-11 card-style-today'>").html(
+          `<div class="card-body card-style-today">
+          <div class="icon">${sunIcon}</div>
+          <h5 class="card-title">${date + "'s Current Weather"}</h5>
+          <p class="card-text card-style">
           <h6>Tempurature: ${temp}</h6>
           <h6>Humidity: ${humidity}</h6>
-        </p>
-        </div>
-        </div>`
+          <h6>Windspeed: ${wind}</h6>
+          </p>
+          </div>`
         );
-        cardRow.append(card);
+        currentTempRow.append(card);
+      }
+
+      if (data.list) {
+        console.log(data.list);
+        // there are 8 three-hour cycles per day, which explains 40 elements in five-day forecast array. Needs to loop differently;
+        for (var i = 4; i < data.list.length; i += 8) {
+          console.log(data.list[i]);
+          var temp = data.list[i].main.temp;
+          var date = data.list[i].dt_txt;
+          date = moment(date).format("dddd");
+          var humidity = data.list[i].main.humidity;
+          var body = $("body");
+          var cardRow = $(".five-day-row");
+          var card = $("<div>").html(
+            `<div class="card flex-row" style="width: 10rem;">
+            <div class="card-body card-style">
+            <div class="icon">${sunIcon}</div>
+            <h5 class="card-title">${date}</h5>
+            <p class="card-text card-style">
+            <h6>Tempurature: ${temp}</h6>
+            <h6>Humidity: ${humidity}</h6>
+            </p>
+            </div>
+            </div>`
+          );
+          cardRow.append(card);
+        } //do not delete
       } //do not delete
     } //do not delete
-  } //do not delete
-
+  }); //do not delete. end of search button function.
 }); // end of document.ready function. do not delete.
 
-// for (var i = 0; i < data.length; i++)
-// ${imageUrl}
-// ${firstName + " " + lastName}
-// ${email}
-// {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+// Next steps of homework:
+// Add weather condition logic for icons
+// Add state or other indicators to grab data
+// setlocalstorage, getlocalstorage to dynamically populate buttons
+// Frontend current weather card and five-day forecast cards need to align
+// Perhaps get rid of navbar to add jumbotron (aesthetic purposes only)
+// Make lat & long dynamic (like the searchButton value var)
+// Get rid of decimals in temp/humidity/etc... values via float? or something like that.
 
-// {coord: {…}, weather: Array(1), base: "stations", main: {…}, visibility: 11265, …}
-
-// {/* <i class="fas fa-bolt"></i>
-// <i class="fas fa-sun"></i>
+// icon reserves below:
+// <i class="fas fa-temperature-high"></i>
 // <i class="fas fa-cloud-showers-heavy"></i>
+// {/* <i class="fas fa-bolt"></i>
 // <i class="fas fa-wind"></i>
 // <i class="fas fa-cloud"></i> */}
 
-  // there are 8 three-hour cycles per day. 5 * 8 = 40;
-  // <i class="fas fa-temperature-high"></i>
+// below is an attept at writing a function to put in logic for the icons.
+// function weatherConditions() {
+//   if (true === true) {
+//     sunIcon;
+//   }
+//   // else if (condition > 70) {
+//   //   cloudIcon;
+//   // }
+// }
+// need to ask T.A.'s about calling function from within a template literal. Can this work? I tried and it didn't, but I feel like it can.
