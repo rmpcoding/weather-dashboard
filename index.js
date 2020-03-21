0ecc31ee3391474ed899f9dcc0012a402060fce0
Robert Parsons <rmpcoding@gmail.com>
2020-03-20 23:23:24 -0700

    adds dynamic lat/lon variables for api calls

--- index.js
+++ index.js
@@ -1,105 +1,102 @@
 $(document).ready(function() {
-  // create function on submit even to store text into variable and into local storage.
+    // create function on submit even to store text into variable and into local storage.
 
-  var searchButton = document.body.querySelector(".search-input");
+    var searchButton = document.body.querySelector('.search-input');
 
-  searchButton.addEventListener("keydown", function(enter) {
-    if (enter.keyCode === 13) {
-      searchButton = searchButton.value;
-    } else return;
+    searchButton.addEventListener('keydown', function(enter) {
+        if (enter.keyCode === 13) {
+            searchButton = searchButton.value;
+        } else return;
 
-    var baseUrl =
-      "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/";
-    var city = searchButton; //change with user input.
-    var apiKey = "a059151d000029215400bdaa7965fbc2";
+        var baseUrl =
+            'https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/';
+        var city = searchButton; //change with user input.
+        var apiKey = 'a059151d000029215400bdaa7965fbc2';
 
-    var lat; // changes with user input
-    var long; // changes with user input
+        var lat; // changes with user input
+        var long; // changes with user input
 
-    // list of icons. More at end of page. Need to work on this as noted at bottom of page.
-    var sunIcon = `<i class="fas fa-sun"></i>`;
-    var cloudIcon = `<i class="fas fa-cloud"></i>`;
+        // list of icons. More at end of page. Need to work on this as noted at bottom of page.
+        var sunIcon = `<i class="fas fa-sun"></i>`;
+        var cloudIcon = `<i class="fas fa-cloud"></i>`;
 
-    var queryUrl = `${baseUrl}weather?q=${city}&units=imperial&appid=${apiKey}`
-    var uvUrl = `${baseUrl}uvi?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`
-    var forecastUrl = `${baseUrl}forecast?q=${city}&units=imperial&appid=${apiKey}`
-    console.log(queryUrl);
+        var queryUrl = `${baseUrl}weather?q=${city}&units=imperial&appid=${apiKey}`;
+        var uvUrl = `${baseUrl}uvi?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
+        var forecastUrl = `${baseUrl}forecast?q=${city}&units=imperial&appid=${apiKey}`;
+        console.log(queryUrl);
 
-    // ajax call function for multiple URLs
-    // ====================================
-    function ajaxCall(weatherApiCall) {
-      $.ajax({
-        url: weatherApiCall,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        generator(response);
-      });
-    }
+        // ajax call function for multiple URLs
+        // ====================================
+        function ajaxCall(weatherApiCall) {
+            $.ajax({
+                url: weatherApiCall,
+                method: 'GET'
+            }).then(function(response) {
+                generator(response);
+            });
+        }
 
-    ajaxCall(queryUrl);
-    ajaxCall(forecastUrl);
-    ajaxCall(uvUrl);
+        ajaxCall(queryUrl);
+        ajaxCall(forecastUrl);
+        ajaxCall(uvUrl);
 
-    function generator(data) {
-      if (data.coord) {
-        lat = data.coord.lat;
-        long = data.coord.lon;
-        console.log(lat);
-        console.log(long);
-      }
+        function generator(data) {
+            if (data.coord) {
+                lat = data.coord.lat;
+                long = data.coord.lon;
+            }
+            if (data.main) {
+                console.log(data.main);
+                var temp = data.main.temp;
+                var humidity = data.main.humidity;
+                var wind = data.wind.speed;
+                date = moment(date).format('dddd');
 
-      if (data.main) {
-        console.log(data.main);
-        var temp = data.main.temp;
-        var humidity = data.main.humidity;
-        var wind = data.wind.speed;
-        date = moment(date).format("dddd");
+                var currentTempRow = $('.current-temp-row');
 
-        var currentTempRow = $(".current-temp-row");
+                var card = $(
+                    "<div class='card col-sm-11 card-style-today'>"
+                ).html(
+                    `<div class="card-body card-style-today">
+                    <div class="icon">${sunIcon}</div>
+                    <h5 class="card-title">${date + "'s Current Weather"}</h5>
+                    <p class="card-text card-style">
+                    <h6>Tempurature: ${temp}&deg</h6>
+                    <h6>Humidity: ${humidity}%</h6>
+                    <h6>Windspeed: ${wind}mph</h6>
+                    </p>
+                    </div>`
+                );
+                currentTempRow.append(card);
+            }
 
-        var card = $("<div class='card col-sm-11 card-style-today'>").html(
-          `<div class="card-body card-style-today">
-          <div class="icon">${sunIcon}</div>
-          <h5 class="card-title">${date + "'s Current Weather"}</h5>
-          <p class="card-text card-style">
-          <h6>Tempurature: ${temp}</h6>
-          <h6>Humidity: ${humidity}</h6>
-          <h6>Windspeed: ${wind}</h6>
-          </p>
-          </div>`
-        );
-        currentTempRow.append(card);
-      }
-
-      if (data.list) {
-        console.log(data.list);
-        // there are 8 three-hour cycles per day, which explains 40 elements in five-day forecast array. Needs to loop differently;
-        for (var i = 4; i < data.list.length; i += 8) {
-          console.log(data.list[i]);
-          var temp = data.list[i].main.temp;
-          var date = data.list[i].dt_txt;
-          date = moment(date).format("dddd");
-          var humidity = data.list[i].main.humidity;
-          var body = $("body");
-          var cardRow = $(".five-day-row");
-          var card = $("<div>").html(
-            `<div class="card flex-row" style="width: 10rem;">
-            <div class="card-body card-style">
-            <div class="icon">${sunIcon}</div>
-            <h5 class="card-title">${date}</h5>
-            <p class="card-text card-style">
-            <h6>Tempurature: ${temp}</h6>
-            <h6>Humidity: ${humidity}</h6>
-            </p>
-            </div>
-            </div>`
-          );
-          cardRow.append(card);
+            if (data.list) {
+                // there are 8 three-hour cycles per day, which explains 40 elements in five-day forecast array. Needs to loop differently;
+                for (var i = 4; i < data.list.length; i += 8) {
+                    console.log(data.list[i]);
+                    var temp = data.list[i].main.temp;
+                    var date = data.list[i].dt_txt;
+                    date = moment(date).format('dddd');
+                    var humidity = data.list[i].main.humidity;
+                    var body = $('body');
+                    var cardRow = $('.five-day-row');
+                    var card = $('<div>').html(
+                        `<div class="card flex-row" style="width: 10rem;">
+                        <div class="card-body card-style">
+                        <div class="icon">${sunIcon}</div>
+                        <h5 class="card-title">${date}</h5>
+                        <p class="card-text card-style">
+                        <h6>Tempurature: ${temp}&deg</h6>
+                        <h6>Humidity: ${humidity}%</h6>
+                        </p>
+                        </div>
+                        </div>`
+                    );
+                    cardRow.append(card);
+                } //do not delete
+            } //do not delete
         } //do not delete
-      } //do not delete
-    } //do not delete
-  }); //do not delete. end of search button function.
+    }); //do not delete. end of search button function.
 }); // end of document.ready function. do not delete.
 
 // Next steps:
